93b98fac114060512c04d7a94fbdc0bb2674aa9b
outafrica <george.jarviz@gmail.com>
2021-11-21 15:54:49 +0300

    Last change

--- resources/js/components/pages/agents/products.vue
+++ resources/js/components/pages/agents/products.vue
@@ -5,7 +5,9 @@
 					
 				<!--~~~~~~~ TABLE ONE ~~~~~~~~~-->
 				<div class="_1adminOverveiw_table_recent _box_shadow _border_radious _mar_b30 _p20">
-					<!-- <p class="_title0">Products <Button @click="addProductModal=true"><Icon type="md-add"/> Add Product</Button></p> -->
+					<p class="_title0">Products 
+						<!-- <Button @click="addProductModal=true"><Icon type="md-add"/> Add Product</Button> -->
+					</p>
 
 					<div class="_overflow _table_div">
 						<table class="_table">
